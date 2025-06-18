84847b8c23e8a01073071cf466e20c72575a96d5
tranvantam310 <52204896+tranvantam310@users.noreply.github.com>
2025-06-18 17:22:11 +0700

    fix tab name

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1408389549"
+    serviceWorkerVersion: "3727405083"
   }
 });
