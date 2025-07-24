f9ef700b06b35484482b18345ccff03fa9b48707
tranvantam310 <52204896+tranvantam310@users.noreply.github.com>
2025-07-24 09:39:19 +0700

    deploy code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3811549214"
+    serviceWorkerVersion: "3539033482"
   }
 });
