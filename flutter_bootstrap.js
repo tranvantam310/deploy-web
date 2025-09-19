a9ddd068a9e3d168e0307b35345e11639c9837a7
tranvantam310 <52204896+tranvantam310@users.noreply.github.com>
2025-09-19 16:18:02 +0700

    cm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2750768904"
+    serviceWorkerVersion: "1310872301"
   }
 });
