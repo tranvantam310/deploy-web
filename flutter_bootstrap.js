a8495bd4f0cc2a98825fec9c4c163d3173af7122
tranvantam310 <52204896+tranvantam310@users.noreply.github.com>
2026-03-11 17:03:55 +0700

    cm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3582911748"
+    serviceWorkerVersion: "3863441396"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4e6e41c142b19c4c2fee22436bacab28",
+const RESOURCES = {"flutter_bootstrap.js": "08c61ab61ba188fdb06cfb95751c8300",
 "version.json": "558eea4b8b4875121c8eb69290c19687",
 "index.html": "6ca8596e69711058037f0ab9ecf91747",
 "/": "6ca8596e69711058037f0ab9ecf91747",
 "CNAME": "bdfe7d1867a752d85709a01ea6517577",
-"main.dart.js": "9ca82676e879fea86031940065ec8ebb",
+"main.dart.js": "2d68827fc96b301c3f4aa37b9ecd487b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "965699335f2b4035e5b9bc94995d471f",
 "icons/Icon-192.png": "89c79d6fafd2b761bccb11f25766ddfb",
