6aed8b8e300c882de54350ba1a7dba68149f7a0f
tranvantam310 <52204896+tranvantam310@users.noreply.github.com>
2026-07-07 17:29:32 +0700

    fix auto play + deploy time

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a22a3b2d8ec23612aa40048b0654d8d0",
+const RESOURCES = {"flutter_bootstrap.js": "559a018a702d00eeb0cf63297fed6723",
 "version.json": "558eea4b8b4875121c8eb69290c19687",
 "index.html": "6ca8596e69711058037f0ab9ecf91747",
 "/": "6ca8596e69711058037f0ab9ecf91747",
 "CNAME": "bdfe7d1867a752d85709a01ea6517577",
-"main.dart.js": "9ac18f150c61514312ba163050a429d0",
+"main.dart.js": "6f94047a2f75c67b1a0d2b414e68a5b1",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "965699335f2b4035e5b9bc94995d471f",
 "icons/Icon-192.png": "89c79d6fafd2b761bccb11f25766ddfb",
